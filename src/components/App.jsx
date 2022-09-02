cbe63a3203d3443cd7d4c5ef01027978bf4aa349
IhorAlexsovich <alexsovich1994@gmail.com>
2022-09-02 10:30:45 +0300

    fin/v2.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { GlobalStyle } from './GlobalStyle';
 import Profile from './Profile/Profile';
-import user from './DataBase/user.json';
+import user from '../DataBase/user.json';
 import Statistics from './Statistics/Statistics';
-import data from './DataBase/data.json';
+import data from '../DataBase/data.json';
 import FriendsList from './FriendList/FriendList';
-import friends from './DataBase/friends.json';
+import friends from '../DataBase/friends.json';
 import TransactionHistory from './Transactions/Transactions';
-import transactions from './DataBase/transactions.json';
+import transactions from '../DataBase/transactions.json';
 
 export const App = () => {
   return (
